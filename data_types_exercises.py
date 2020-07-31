2a4832d249f6d216b4b1e92e68b2299b34775da7
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2020-07-31 14:18:01 -0500

    adding data types exercises to github

---
+++ data_types_exercises.py
@@ -0,0 +1,35 @@
+#You have rented some movies for your kids: The little mermaid (for 3 days), 
+# Brother Bear (for 5 days, they love it), 
+# and Hercules (1 day, you don't know yet if they're going to like it). 
+# If price for a movie per day is 3 dollars, how much will you have to pay?#p=3 'price per day
+#littlemer=3 'days'
+#brobear=5 'days'
+#hercules=1 'day'
+#totalprice= littlemer*p + brobear*p + hercules*3
+
+#Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, 
+# they pay you a different rate per hour. 
+# Google pays 400 dollars per hour, Amazon 380, and Facebook 350. 
+# How much will you receive in payment for this week? 
+# You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+#h=hours
+#g= 6 'hours'
+#f=10 'hours'
+#a=4 'hours'
+#gpay=400
+#fpay=350
+#apay=380
+#totalpay=g*gpay + f*fpay + a*apay
+
+#A student can be enrolled to a class only if the class is not full 
+# and the class schedule does not conflict with her current schedule.
+#return true if 
+#class<full and classtime!=another class time
+
+#A product offer can be applied only if people buys more than 2 items,
+#and the offer has not expired. 
+#Premium members do not need to buy a specific amount of products.
+#return true if
+#(premium member or buy>2 items)
+#and offer!=expire
+
