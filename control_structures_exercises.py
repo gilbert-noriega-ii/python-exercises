fee4948e0148a5a4acfc6857e43f320ba4fbf5bd
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2020-08-03 08:59:46 -0500

    completed problem 2

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -51,3 +51,49 @@ if number_of_hours_worked > 40:
 else:
     paycheck = number_of_hours_worked * hourly_rate
 print (f"If you worked {number_of_hours_worked} hours at ${hourly_rate} then your paycheck will be ${paycheck}")
+
+#2a)While
+# Create an integer variable i with a value of 5.
+# Create a while loop that runs so long as i is less than or equal to 15
+# Each loop iteration, output the current value of i, then increment i by one.
+i = 5
+while i <= 15:
+    print (i)
+    i += 1
+
+#Create a while loop that will count by 2's starting with 0 and ending at 100.
+#Follow each number with a new line.
+j = 0
+while j <= 100:
+    print(j)
+    j += 2
+
+#Alter your loop to count backwards by 5's from 100 to -10
+j = 0
+while j < 100:
+    print(j)
+    j += 2
+while j >= -10:
+    print(j)
+    j -= 5
+
+#Create a while loop that starts at 2, 
+# and displays the number squared on each line 
+# while the number is less than 1,000,000
+k = 2
+while k < 1000000:
+    print(k)
+    k = k ** 2
+
+#Write a loop that uses print to create the output shown below.
+l = 100
+while l > 0:
+    print(l)
+    l -= 5
+
+#2b)For Loops
+#Write some code that prompts the user for a number,
+#then shows a multiplication table up through 10 for that number.
+
+
+
