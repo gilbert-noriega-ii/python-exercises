32771350726db19e47a9b0bec15d9de6bdf3a67b
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2020-08-06 08:56:16 -0500

    finished import exercises

--- import_exercises.py
+++ import_exercises.py
@@ -30,7 +30,7 @@ len(list(comb('abcd',2))) #give the amount of 2 different letter combinations fo
 #it will produce a list of dictionaries. 
 #Using this data, write some code that calculates and 
 #outputs the following information:
-from json import * # importing json and all modules
+import json # importing json and all modules
 prof = open('profiles.json') # storing the open json file command as variable prof
 data = json.load(prof) #storing the loading of json file command as variable data
 
@@ -45,3 +45,39 @@ print(f'The total number of active users is {number_of_active_users}.') # print 
 #Number of inactive users
 number_of_inactive_users = len([profile['isActive'] for profile in data if profile['isActive'] == False]) ##counting number of profiles in dictionary called data if isActive equals true
 print(f'The total number of active users is {number_of_inactive_users}.') # print our results
+
+#Grand total of balances for all users
+total_balance = 0
+for profile in data:       
+    total_balance += float(profile['balance'].replace(",", '').replace("$", "")) 
+print(f'The grand total of balances for all user is ${round(total_balance, 2)}.')
+
+#Average balance per user
+total_average = round(total_balance / number_of_users, 2)
+print(f'The overall average balance per user is ${total_average}.')
+
+#User with the lowest balance
+lowest_balance = [profile['name'] for profile in data if profile['balance'] == min([profile['balance'] for profile in data])] 
+print(f"The user with the lowest balance is {lowest_balance[0]}.")
+
+#User with the highest balance
+highest_balance = [profile['name'] for profile in data if profile['balance'] == max([profile['balance'] for profile in data])] 
+print(f"The user with the highest balance is {highest_balance[0]}.")
+
+#Most common favorite fruit
+favorite_fruits = [profile['favoriteFruit'] for profile in data]
+fruit_counts = {}
+for fruit in favorite_fruits:
+    if fruit in fruit_counts:
+        fruit_counts[fruit] += 1
+    else:
+        fruit_counts[fruit] = 1
+print(f"The most common favorite fruit is {max(fruit_counts)}.")
+
+#Least most common favorite fruit
+print(f"The least common favorite fruit is {min(fruit_counts)}.")
+
+#Total number of unread messages for all users
+unread_messages = [int(profile['greeting'].lower().strip("abcdefghijklmonpqrstuvwxyz!,. ")) for profile in data if 'unread' in profile['greeting']]
+total_messages = sum(unread_messages) 
+print(f'There are {total_messages} unread messages for all users.')
